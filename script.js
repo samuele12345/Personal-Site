581ae323c91480a60ebbea7bed8aae4eb0d92056
samuele12345 <samueleriva2016@gmail.com>
2026-01-31 16:44:57 +0100

    Aggiunta pagina contact, con link e paragrafi necessari, aggiunto script per chiudere i menu dropdown se si preme su un link in essi contenuto. modifiche ai link header di home e projects.

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const carousel = document.querySelector(".carousel");
 const rightBtn = document.querySelector(".arrow.right");
 const leftBtn  = document.querySelector(".arrow.left");
 
+// Seleziona tutti i link all'interno della nav
+const navLinks = document.querySelectorAll('#nav-div a');
+
+// Seleziona tutti i dettagli nella nav
+const detailsElements = document.querySelectorAll('#nav-div details');
+
 // Quando clicco sulla freccia destra, il carosello scorre di 320px verso destra
 rightBtn.addEventListener("click", () => {
   carousel.scrollBy({
@@ -20,5 +26,15 @@ leftBtn.addEventListener("click", () => {
   carousel.scrollBy({
     left: -320,      // distanza negativa per scorrere a sinistra
     behavior: "smooth" // scorrimento animato graduale
+  });
+});
+
+// Aggiungi un listener a ogni link
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    // Chiudi tutti i dettagli aperti
+    detailsElements.forEach(detail => {
+      detail.removeAttribute('open');
+    });
   });
 });
